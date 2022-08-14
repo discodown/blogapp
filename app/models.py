b70b752d4031fcfab4c1f993776617c1b21e743d
Marshall Walsh <tum92783@temple.edu>
2022-08-14 15:15:02 -0400

    Updated db models to support post tags

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ import bleach
 from flask import current_app, request, url_for
 from . import db
 
+post_tags = db.Table('post_tags',
+            db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+            db.Column('entry_id', db.Integer, db.ForeignKey('entry.id'))
+)
+
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,3 +17,12 @@ class Post(db.Model):
     body_html = db.Column(db.Text)
     time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author = db.Column(db.String())
+    tags = db.relationship('Tag', secondary=post_tags,
+            backref=db.backref('posts', lazy='dynamic')
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(primary_key=True)
+    name = db.Column(db.String())
+
+    def __repr__(self):
+        return '<Tag %s>' % self.name
