66d744bcd97be52e99df410fc58a9c0c77c11ca4
Marshall Walsh <tum92783@temple.edu>
2022-08-16 16:01:16 -0400

    Looking up post by tags works now

--- app/main/views.py
+++ app/main/views.py
@@ -21,3 +21,18 @@ def post(id):
     post_tags = post.get_tags()
     sidebar_tags = sorted(Tag.query.all(), key=lambda tag: tag.name)
     return render_template('post.html', post=post, post_tags=post_tags, sidebar_tags=sidebar_tags, recent=recent)
+
+@main.route('/tagged/<tag>', methods=['GET', 'POST'])
+def tagged(tag):
+    #Might need to ensure they are sorted by date?
+    tagged = Tag.query.get(tag).get_posts()
+    page = request.args.get('page', 1, type=int)
+
+    pagination = tagged.order_by(Post.time.desc()).paginate(
+        page, per_page=current_app.config['BLOG_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    recent=Post.query.order_by(Post.time.desc())[0:5]
+    sidebar_tags = sorted(Tag.query.all(), key=lambda tag: tag.name)
+    return render_template('tagged.html', posts=posts, pagination=pagination, sidebar_tags=sidebar_tags, recent=recent, tag=tag, page=page)
+
